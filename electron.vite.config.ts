050eb7272bef9759ce117059cf98e676063abd7f
eKoh951 <greatekoh@gmail.com>
2024-06-10 17:25:50 -0600

    Reseted electron conifg

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,43 +1,18 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
 
 export default defineConfig({
   main: {
-    plugins: [
-      externalizeDepsPlugin(),
-      nodePolyfills({
-        globals: {
-          Buffer: true,
-          global: true,
-          process: true
-        },
-        protocolImports: true
-      })
-    ]
+    plugins: [externalizeDepsPlugin()]
   },
   preload: {
-    plugins: [
-      externalizeDepsPlugin(),
-      nodePolyfills({
-        globals: {
-          Buffer: true,
-          global: true,
-          process: true
-        },
-        protocolImports: true
-      })
-    ]
+    plugins: [externalizeDepsPlugin()]
   },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src'),
-        webtorrent: fileURLToPath(
-          new URL('./node_modules/webtorrent/dist/webtorrent.min.js', import.meta.url)
-        )
+        '@renderer': resolve('src/renderer/src')
       }
     },
     plugins: [react()]
